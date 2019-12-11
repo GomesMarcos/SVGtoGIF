e6e3d6620ea34ac06092ca6cfb2b739c14cb9ac2
GomesMarcos <gomes.marcosjf@gmail.com>
2019-12-11 17:28:01 -0300

    WIP render frames in canvas

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,6 @@
-// const svg = document.getElementsByTagName('svg');
-// let img = svg.toDataURL('image/png');
-
-// document.write('<img src="' + img + '"/>');
-
-var svg = $('#source')[0];
-var target = $('#target');
-var getCanvas;
-var imageData = getCanvas.toDataURL("image/png");
-var newData = imageData.replace(/^data/image\/png/, "data:application/octet-stream");
-$("#btnRender").attr('download', 'pic.png').attr("href", newData);
-
-function renderImage() {
-  console.log(svg, 'qqwe1');
-  
-  html2canvas(svg, {
-    onrenreded: function (canvas) {
-      $("#target").append(canvas);
-      getCanvas = canvas;
-      console.log('qwe2');
-    }
+function getScreenshot() {
+  html2canvas($('svg')[0], function(canvas) {
+    $('#result').html("");
+    $('#result').append(canvas);
   })
 }
